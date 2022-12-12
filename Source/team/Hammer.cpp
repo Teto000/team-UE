2ad5eb7a48c3b504c7a7a9c719c292aa633497d3
Teruto Sato <211052@st.yohida-g.ac.jp>
2022-12-12 15:34:16 +0900

    ハンマー追加

---
+++ Source/team/Hammer.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Hammer.h"
+
+// Sets default values
+AHammer::AHammer()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHammer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHammer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AHammer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//----------------------------
+	// 移動
+	//----------------------------
+	// MoveForwardの入力値をMoveForward関数へバインドする
+	InputComponent->BindAxis("MoveForward", this, &AHammer::MoveForward);
+
+	// MoveRightの入力値をMoveRight関数へバインドする
+	InputComponent->BindAxis("MoveRight", this, &AHammer::MoveRight);
+}
+
